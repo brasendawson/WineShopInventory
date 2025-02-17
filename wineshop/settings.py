a295e24d80388050f6979a1cbe591b0103a3a645
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2025-02-17 00:59:23 +0000

    update

--- wineshop/settings.py
+++ wineshop/settings.py
@@ -79,18 +79,13 @@ WSGI_APPLICATION = 'wineshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wine_shop',
-        'USER': 'root',
-        'PASSWORD': 'E8xfT#jjgc!5G@s',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
